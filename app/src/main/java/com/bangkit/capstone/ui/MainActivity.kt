b65348b12e757fc41330b21f6e026d4412c9b6c6
Dokari4 <alheta.matalarens@gmail.com>
2022-06-12 23:06:42 +0700

    added Custom ActionBar

--- app/src/main/java/com/bangkit/capstone/ui/MainActivity.kt
+++ app/src/main/java/com/bangkit/capstone/ui/MainActivity.kt
@@ -2,12 +2,15 @@ package com.bangkit.capstone.ui
 import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.asLiveData
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bangkit.capstone.R
 import com.bangkit.capstone.database.Message
 import com.bangkit.capstone.databinding.ActivityMainBinding
 import com.bangkit.capstone.helper.DateHelper
@@ -28,11 +31,35 @@ class MainActivity : AppCompatActivity() {
     private val binding get() = _binding
 
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_top_app_bar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.clear -> {
+                Toast.makeText(this, "Item Selected", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            else -> return true
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+
+        //Set Custom Action Bar
+        supportActionBar?.show()
+        supportActionBar?.setDisplayShowCustomEnabled(true)
+        supportActionBar?.setCustomView(R.layout.custom_toolbar)
+        //Clickable Action Bar
+        setOnClickToolbar()
+
         checker = Checker(applicationContext)
         observeOnboard()
         networkCheck()
@@ -95,6 +122,14 @@ class MainActivity : AppCompatActivity() {
             .ofFloat(this, View.ALPHA, if (isVisible) 1f else 0f)
             .setDuration(duration)
             .start()
+    }
+
+    //toolbar Click Feedback
+    private fun setOnClickToolbar() {
+        val toolbar = findViewById<View>(R.id.toolbarCustom)
+        toolbar.setOnClickListener {
+            Toast.makeText(this, "Toolbar Clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 
     //Splash Stuff
