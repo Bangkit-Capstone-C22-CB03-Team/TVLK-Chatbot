04aec92f42e757cce28f060dd6316c6459d855ce
Haf0 <hafit.abekrori@students.amikom.ac.id>
2022-06-15 23:53:01 +0700

    adding text to speech and speech to text

--- app/src/main/java/com/bangkit/capstone/ui/MainActivity.kt
+++ app/src/main/java/com/bangkit/capstone/ui/MainActivity.kt
@@ -2,10 +2,13 @@ package com.bangkit.capstone.ui
 import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.speech.tts.TextToSpeech
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
@@ -18,6 +21,8 @@ import com.bangkit.capstone.helper.DateHelper
 import com.bangkit.capstone.viewmodel.CheckInternetAccess
 import com.bangkit.capstone.viewmodel.MainViewModel
 import com.bangkit.capstone.viewmodel.ViewModelFactory
+import java.util.*
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainViewModel : MainViewModel
@@ -26,7 +31,90 @@ class MainActivity : AppCompatActivity() {
     private lateinit var checkInternetAccess: CheckInternetAccess
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding
+    private var  category : Int by Delegates.notNull()
+    private var isAccessbility : Boolean by Delegates.notNull()
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+
+        category = intent.getIntExtra(CATEGORY, 0)
+        isAccessbility = intent.getBooleanExtra(ACCESSBILITY, true)
+
+        if(isAccessbility){
+            binding?.buttonsContainer?.isVisible = false
+            binding?.blindContainer?.isVisible = true
+        }else{
+            binding?.buttonsContainer?.isVisible = true
+            binding?.blindContainer?.isVisible = false
+        }
+
+
+
+        //Set Custom Action Bar
+        supportActionBar?.show()
+        supportActionBar?.setDisplayShowCustomEnabled(true)
+        supportActionBar?.setCustomView(R.layout.custom_toolbar)
+
+
+        setViewModel()
+        networkCheck()
+        setAdapter()
+
+        binding?.loading?.animateVisibility(false)
+        binding?.sendButtonChatLog?.setOnClickListener {
+            binding?.loading?.visibility = View.VISIBLE
+            message = binding?.edittextChatLog?.text.toString()
+            mainViewModel.isLoading.observe(this){
+                binding?.loading?.animateVisibility(it)
+            }
+            val msg = message.trim()
+            if(binding?.isConnected?.visibility == View.GONE){
+                if (msg!=""){
+                    userChat(msg)
+                    mainViewModel.getBotResponse(msg, category,isAccessbility)
+                    binding?.edittextChatLog?.setText("")
+                }else{
+                    if (isAccessbility){
+                        mainViewModel.speak("Tidak boleh kosong")
+                    }else{
+                        Toast.makeText(this, "tidak boleh kosong", Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+            }else{
+                SweetAlertDialog(this, SweetAlertDialog.ERROR_TYPE)
+                    .setTitleText(getString(R.string.connection_dialog_title))
+                    .setContentText(getString(R.string.connection_dialog_content))
+                    .setConfirmText(getString(R.string.connection_dialog_confirm))
+                    .show()
+            }
+        }
+
+        if(isAccessbility){
+            mainViewModel.textMessage.observe(this){
+                speak(it)
+            }
+        }
+
+        binding?.askButton?.setOnClickListener {
+            mainViewModel.displaySpeechRecognizer()
+        }
+
+        binding?.backButton?.setOnClickListener {
+            moveToCategory()
+        }
+
+        mainViewModel.getAllMessages().observe(this){
+            adapter.setList(it)
+            binding?.recyclerviewChat?.scrollToPosition(adapter.itemCount-1)
+        }
+
+    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater = menuInflater
@@ -43,65 +131,6 @@ class MainActivity : AppCompatActivity() {
             else -> return true
         }
     }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-
-        val category = intent.getIntExtra(CATEGORY, 0)
-
-        _binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding?.root)
-
-
-        //ini nanti buat accessbility yakin gas?
-        binding?.buttonsContainer?.isVisible = false
-
-        //Set Custom Action Bar
-        supportActionBar?.show()
-        supportActionBar?.setDisplayShowCustomEnabled(true)
-        supportActionBar?.setCustomView(R.layout.custom_toolbar)
-
-
-
-        networkCheck()
-        setAdapter()
-
-        binding?.loading?.animateVisibility(false)
-        binding?.sendButtonChatLog?.setOnClickListener {
-            binding?.loading?.visibility = View.VISIBLE
-            message = binding?.edittextChatLog?.text.toString()
-            mainViewModel.isLoading.observe(this){
-                binding?.loading?.animateVisibility(it)
-            }
-            val msg = message.trim()
-            if(binding?.isConnected?.visibility == View.GONE){
-                if (msg =="exit"||msg=="quit"||msg=="keluar"){
-                    moveToCategory()
-                }else {
-                    if (msg!=""){
-                        userChat(msg)
-                        mainViewModel.getBotResponse(msg, category)
-                        binding?.edittextChatLog?.setText("")
-                    }else{
-                        Toast.makeText(this, "tidak boleh kosong", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }else{
-                SweetAlertDialog(this, SweetAlertDialog.ERROR_TYPE)
-                    .setTitleText(getString(R.string.connection_dialog_title))
-                    .setContentText(getString(R.string.connection_dialog_content))
-                    .setConfirmText(getString(R.string.connection_dialog_confirm))
-                    .show()
-            }
-        }
-
-        mainViewModel.getAllMessages().observe(this){
-            adapter.setList(it)
-            binding?.recyclerviewChat?.scrollToPosition(adapter.itemCount-1)
-        }
-
-    }
     private fun userChat(message: String){
         mainViewModel.insert(
             Message(message, "user",DateHelper.getCurrentDate(),0)
@@ -115,10 +144,15 @@ class MainActivity : AppCompatActivity() {
         adapter = ChatAdapter()
         binding?.recyclerviewChat?.layoutManager = LinearLayoutManager(this)
         binding?.recyclerviewChat?.adapter = adapter
-        mainViewModel = obtainViewModel(this)
+
     }
 
-    fun networkCheck(){
+    private fun setViewModel(){
+        mainViewModel = obtainViewModel(this)
+        mainViewModel.initialForSpeech(textToSpeechEngine,startForResult)
+    }
+
+    private fun networkCheck(){
         checkInternetAccess = CheckInternetAccess(application)
         checkInternetAccess.observe(this){isConnected->
             if(isConnected){
@@ -143,8 +177,34 @@ class MainActivity : AppCompatActivity() {
         finish()
     }
 
+
+    //Accessibility
+    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val spokenText = result.data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                .let { text-> text?.get(0) }
+
+            if(spokenText!=null){
+                userChat(spokenText)
+                mainViewModel.getBotResponse(spokenText, category,isAccessbility)
+            }
+        }
+    }
+
+    private val textToSpeechEngine: TextToSpeech by lazy {
+        TextToSpeech(this) {
+            if (it == TextToSpeech.SUCCESS) textToSpeechEngine.language = Locale("in_ID")
+        }
+    }
+
+    private fun speak(msg: String){
+        mainViewModel.speak(msg)
+    }
+
+
     companion object {
         const val CATEGORY = "category"
+        const val ACCESSBILITY = "ACCESSBILITY"
     }
 
 
--- app/src/main/java/com/bangkit/capstone/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/bangkit/capstone/viewmodel/MainViewModel.kt
@@ -1,28 +1,42 @@
 package com.bangkit.capstone.viewmodel
 
 import android.app.Application
+import android.content.Intent
+import android.speech.RecognizerIntent
+import android.speech.tts.TextToSpeech
 import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.bangkit.capstone.database.Message
 import com.bangkit.capstone.database.MessageRepository
 import com.bangkit.capstone.helper.DateHelper
 import com.bangkit.capstone.network.ApiConfig
 import com.bangkit.capstone.network.BotResponse
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.*
 
 class MainViewModel(application: Application) : ViewModel() {
+
+    private lateinit var textToSpeechEngine: TextToSpeech
+    private lateinit var startForResult: ActivityResultLauncher<Intent>
+
 
     private val mMessageRepository: MessageRepository = MessageRepository(application)
 
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> = _isLoading
 
+    private val _textMessage = MutableLiveData<String>()
+    val textMessage: LiveData<String> = _textMessage
 
-    fun getBotResponse(message:String, categid:Int){
+
+    fun getBotResponse(message:String, categid:Int, isAccessbility: Boolean){
         _isLoading.value = true
         val client = ApiConfig.getApiService().getResponse(message,  categid)
         client.enqueue(object : Callback<BotResponse>{
@@ -31,12 +45,16 @@ class MainViewModel(application: Application) : ViewModel() {
                     _isLoading.value = false
                     val msg = response.body()!!.botResponse
                     insert(Message(msg,"bot",DateHelper.getCurrentDate(),0))
+                    _textMessage.value = msg
+                    Log.d("spekingtok", "${textMessage.value}")
+
                 }
             }
 
             override fun onFailure(call: Call<BotResponse>, t: Throwable) {
                 _isLoading.value = false
                 Log.d("error get bot response", t.message.toString())
+                insert(Message("Bot sedang error silahkan tunggu","bot",DateHelper.getCurrentDate(),0))
             }
 
         }
@@ -51,5 +69,31 @@ class MainViewModel(application: Application) : ViewModel() {
         mMessageRepository.deleteAll()
     }
 
+
     fun getAllMessages(): LiveData<List<Message>> = mMessageRepository.getAllMessages()
+
+
+    fun initialForSpeech(
+        engine: TextToSpeech, launcher: ActivityResultLauncher<Intent>
+    ) = viewModelScope.launch {
+        textToSpeechEngine = engine
+        startForResult = launcher
+    }
+
+    fun displaySpeechRecognizer() {
+        startForResult.launch(Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+            putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale("in_ID"))
+            putExtra(RecognizerIntent.EXTRA_PROMPT, Locale("Bicara sekarang"))
+        })
+    }
+
+    fun speak(text: String) = viewModelScope.launch{
+        textToSpeechEngine.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
+    }
+
+
 }
