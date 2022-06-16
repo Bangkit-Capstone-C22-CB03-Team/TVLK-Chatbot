dc7d091cca1304e89e8a9d3572039b0954593582
Haf0 <hafit.abekrori@students.amikom.ac.id>
2022-06-16 20:41:48 +0700

    delete sapa category

--- app/src/main/java/com/bangkit/capstone/ui/CategoryActivity.kt
+++ app/src/main/java/com/bangkit/capstone/ui/CategoryActivity.kt
@@ -24,12 +24,11 @@ class CategoryActivity : AppCompatActivity() {
     }
 
     private fun setCardClick() {
-        binding.cardView1.setOnClickListener { moveMain(0) }
-        binding.cardView2.setOnClickListener { moveMain(1) }
-        binding.cardView3.setOnClickListener { moveMain(2) }
-        binding.cardView4.setOnClickListener { moveMain(3) }
-        binding.cardView5.setOnClickListener { moveMain(4) }
-        binding.cardView6.setOnClickListener { moveMain(5) }
+        binding.cardView1.setOnClickListener { moveMain(1) }
+        binding.cardView2.setOnClickListener { moveMain(2) }
+        binding.cardView3.setOnClickListener { moveMain(3) }
+        binding.cardView4.setOnClickListener { moveMain(4) }
+        binding.cardView5.setOnClickListener { moveMain(5) }
     }
 
 
